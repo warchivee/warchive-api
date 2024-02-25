ab9354cf3a3697d959d2468fee2337c9f522724e
ej522 <jsej07@gmail.com>
2024-02-25 20:39:35 +0900

    feat: make collection entity, collectionItem entity
    Co-authored-by: ej522 <jsej07@gmail.com>
    Co-authored-by: jingoone <jingooneh@gmail.com>

--- src/collection/entities/collection.entity.ts
+++ src/collection/entities/collection.entity.ts
@@ -1,1 +1,11 @@
-export class Collection {}
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'collection' })
+export class Collection extends CommonEntity {
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ length: 200 })
+  note: string;
+}
