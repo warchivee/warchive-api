a29740c349baeacf4e3ae63066702bba3c3ec957
ej522 <jsej07@gmail.com>
2024-03-04 23:50:18 +0900

    collection-item entity 수정

--- src/collection/entities/collection-item.entity.ts
+++ src/collection/entities/collection-item.entity.ts
@@ -1,16 +1,11 @@
 // import { CommonEntity } from 'src/common/entities/common.entity';
-import { ManyToOne, JoinColumn, Entity, PrimaryColumn } from 'typeorm';
+import { ManyToOne, JoinColumn, Entity } from 'typeorm';
 import { Collection } from './collection.entity';
 import { Wata } from 'src/admin/wata/entities/wata.entity';
+import { CommonEntity } from 'src/common/entities/common.entity';
 
 @Entity({ name: 'collection_item' })
-export class CollectionItem {
-  @PrimaryColumn()
-  collection_id: number;
-
-  @PrimaryColumn()
-  wata_id: number;
-
+export class CollectionItem extends CommonEntity {
   @ManyToOne(() => Collection, (collection) => collection.id)
   @JoinColumn({ name: 'collection_id' })
   collection: Collection;
