1fe2916512e01180b8a36a2adc4f9e7bfdcc384f
ej522 <jsej07@gmail.com>
2024-03-29 11:38:44 +0900

    불필요한 주석 제거

--- src/collection/dto/add-collection-item.dto.ts
+++ src/collection/dto/add-collection-item.dto.ts
@@ -3,14 +3,6 @@ import { Type } from 'class-transformer';
 import { IsArray, IsNumber } from 'class-validator';
 
 export class AddCollectionItemDto {
-  // @ApiProperty({
-  //   description: '컬렉션 ID',
-  //   example: '1',
-  //   required: true,
-  // })
-  // @IsNumber()
-  // collection_id: number;
-
   @ApiProperty({
     description: '와타 ID',
     example: '1',
