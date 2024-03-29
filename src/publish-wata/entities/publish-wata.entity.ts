49b0ec7914641e9a0a359b7bf427049b508f20e2
jwkcs <jiwonkim652@gmail.com>
2024-03-29 12:43:43 +0000

    #24 feat: publish-wata.entity.ts

--- src/publish-wata/entities/publish-wata.entity.ts
+++ src/publish-wata/entities/publish-wata.entity.ts
@@ -1,1 +1,34 @@
-export class PublishWata {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity({ name: 'published_wata' })
+export class PublishWata {
+    @PrimaryColumn()
+    id: number;
+
+    @Column({ length: 250 })
+    title: string;
+  
+    @Column({ length: 250, nullable: true })
+    creators: string;
+
+    @Column({ length: 250, nullable: true })
+    thumbnail_card?: string;
+
+    @Column({ length: 250, nullable: true })
+    thumbnail_book?: string;
+
+    @Column({ type: 'json', nullable: true })
+    categories?: string[];
+
+    @Column({ type: 'json', nullable: true })
+    genre?: string[];
+    
+    @Column({ type: 'json', nullable: true })
+    keywords?: string[];
+    
+    @Column({ type: 'json', nullable: true })
+    cautions?: string[];
+    
+    @Column({ type: 'json', nullable: true })
+    platforms?: string[];
+}
