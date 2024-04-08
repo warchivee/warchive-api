78dfaafd9624bc116a0f5a8aa9a5c777d72f2b8e
ej522 <jsej07@gmail.com>
2024-04-08 21:43:53 +0900

    feat: publish api 추가

--- src/publish-wata/entities/publish-wata.entity.ts
+++ src/publish-wata/entities/publish-wata.entity.ts
@@ -1,34 +1,57 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'published_wata' })
 export class PublishWata {
   @PrimaryColumn()
   id: number;
 
-    @Column({ length: 250 })
-    title: string;
-  
-    @Column({ length: 250, nullable: true })
-    creators: string;
+  @Column({ length: 250 })
+  title: string;
 
-    @Column({ length: 250, nullable: true })
-    thumbnail_card?: string;
+  @Column({ length: 250, nullable: true })
+  creators: string;
 
-    @Column({ length: 250, nullable: true })
-    thumbnail_book?: string;
+  @Column({ length: 250, nullable: true })
+  thumbnail_card?: string;
 
-    @Column({ type: 'json', nullable: true })
-    categories?: string[];
+  @Column({ length: 250, nullable: true })
+  thumbnail_book?: string;
 
-    @Column({ type: 'json', nullable: true })
-    genre?: string[];
-    
-    @Column({ type: 'json', nullable: true })
-    keywords?: string[];
-    
-    @Column({ type: 'json', nullable: true })
-    cautions?: string[];
-    
-    @Column({ type: 'json', nullable: true })
-    platforms?: string[];
+  @Column({ type: 'json', nullable: true })
+  categories?: string[];
+
+  @Column({ type: 'json', nullable: true })
+  genre?: string[];
+
+  @Column({ type: 'json', nullable: true })
+  keywords?: string[];
+
+  @Column({ type: 'json', nullable: true })
+  cautions?: string[];
+
+  @Column({ type: 'json', nullable: true })
+  platforms?: string[];
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'adder_id' })
+  adder: User;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'updater_id' })
+  updater: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
 }
